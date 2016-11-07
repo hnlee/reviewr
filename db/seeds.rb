a2675fbf0ae6c3560d165f366cb453a03d8d3229
Nicole <nicolecarpenterusa@gmail.com>
2016-11-07 15:47:01 -0600

    adds user db relationships to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 coding_projects = ['HTTP Server', 'Hangman', 'TTT', 'Bowling Game', 'Coin Changer', 'Roman Numerals', 
-          'Conways Game of Life', 'Anagrams', 'Prime Factors', 'Mastermind']
+          'Conway\'s Game of Life', 'Anagrams', 'Prime Factors', 'Mastermind']
 
 languages = ['Java', 'Ruby', 'Clojure', 'Python', 'Haskell', 'Elixir', 'Elm', 'C#', 'Go', 'Rust', 'Javascript']
 
-greetings = ['Hey there! ', 'Well hello there. ', 'Hi. ', 'Youre here! ', 'Greetings. ', 'Welcome. ']
+greetings = ['Hey there! ', 'Well hello there. ', 'Hi. ', 'You\'re here! ', 'Greetings. ', 'Welcome. ']
 
 schmooze =  ['I worked really hard on this project', 
             'I am really excited for you to check out my project', 
@@ -18,31 +18,31 @@ schmooze =  ['I worked really hard on this project',
             'The fact that you are taking time out of your busy, busy day means so very much to me', 
             'This is the first time I have had the opportunity to work with this technology']
 
-userNames = ['barackobama', 'hillaryclinton', 'donaldtrump', 'berniesanders', 'tedcruz', 'jebbush', 'marcorubio', 'lincolnchafee',
+user_names = ['barackobama', 'hillaryclinton', 'donaldtrump', 'berniesanders', 'tedcruz', 'jebbush', 'marcorubio', 'lincolnchafee',
              'martinomalley', 'jimwebb', 'ricksantorum', 'scottwalker', 'chrischristie', 'bencarson']
 
-content = [ 'This looks great', 
-            'This looks awful', 
-            'This looks really good', 
-            'This needs work',
-            'One thing I really liked was how you were able to capture the mood of the reader', 
-            'One thing I did not think was great was the way that your indentations are inconsistent',
-            'This shows a lot of potential', 
-            'This shows no potential', 
-            'This is not at all what I was expecting', 
-            'While it certainly clever, it is not obvious what you are trying to do, not very readable',
-            'I dont really understand the premise here. Maybe you should try to explain this out loud to someone and see if it makes sense.',
-            'Hey this looks great! I dont even know what to tell you to fix, it is perfect, just like you!',
-            'Did you plagiarize this? This looks oddly familiar...', 
-            'Reading this is giving me a headache', 
-            'This is really good, I can tell that you have made a lot of changes from the previous iteration and it shows. You addressed the spacing issue, but I am not sure that my feedback about the font was taken correctly. Maybe we can do an in person meeting and I can explain what I meant.', 
-            'Wow!', 
-            'You should move line 43 to a separate file, it is creating a clear violation of the Single Responsibility Principle', 
-            'By including line 112, you are creating a violation of the Dependency Inversion Principle', 
-            'I am really impressed by your use of the Open-Closed Principle',
-            'Great use of the Liskov Substitution Principle here', 
-            'The way that this is written, it violates the Interface Segregation Principle. I would fix this if I were you.',
-            'This looks really great. It is obvious that you are really considering your SOLID principles here']
+review_content = ['This looks great', 
+                  'This looks awful', 
+                  'This looks really good', 
+                  'This needs work',
+                  'One thing I really liked was how you were able to capture the mood of the reader', 
+                  'One thing I did not think was great was the way that your indentations are inconsistent',
+                  'This shows a lot of potential', 
+                  'This shows no potential', 
+                  'This is not at all what I was expecting', 
+                  'While it certainly clever, it is not obvious what you are trying to do, not very readable',
+                  'I dont really understand the premise here. Maybe you should try to explain this out loud to someone and see if it makes sense.',
+                  'Hey this looks great! I dont even know what to tell you to fix, it is perfect, just like you!',
+                  'Did you plagiarize this? This looks oddly familiar...', 
+                  'Reading this is giving me a headache', 
+                  'This is really good, I can tell that you have made a lot of changes from the previous iteration and it shows. You addressed the spacing issue, but I am not sure that my feedback about the font was taken correctly. Maybe we can do an in person meeting and I can explain what I meant.', 
+                  'Wow!', 
+                  'You should move line 43 to a separate file, it is creating a clear violation of the Single Responsibility Principle', 
+                  'By including line 112, you are creating a violation of the Dependency Inversion Principle', 
+                  'I am really impressed by your use of the Open-Closed Principle',
+                  'Great use of the Liskov Substitution Principle here', 
+                  'The way that this is written, it violates the Interface Segregation Principle. I would fix this if I were you.',
+                  'This looks really great. It is obvious that you are really considering your SOLID principles here']
 
 thumbs_up = [ '',
               'This is a good review',
@@ -56,27 +56,40 @@ thumbs_down = [ 'This is not kind',
                 'This is neither kind nor actionable',
                 'This is neither kind, specific, or actionable' ]
 
-titles = []
+project_count = 20
 
-descriptions = []
-
-20.times do
-  titles << languages.sample + ' ' + coding_projects.sample
+def get_random_item_index(collection)
+  rand(collection.length) + 1
 end
 
-20.times do |i|
-  description = (greetings.sample + "I want to invite you to check out my " + titles[i] + ". " + schmooze.sample + ". You can check out my repo here: www.github.com/" + userNames.sample + "/" + titles[i].gsub(/\s+/, "-") + ". Thanks in advance for your feedback!")
-  descriptions << description
+user_names.each do |name|
+  User.create(name: name, email: name + '@gmail.com', password: 'password')
 end
 
-projects = titles.zip(descriptions)
-
-projects.each do |title, description|
+project_count.times do |i|
+  title = languages.sample + ' ' + coding_projects.sample
+  description = (greetings.sample + "I want to invite you to check out my " + title + ". " + schmooze.sample + ". You can check out my repo here: www.github.com/" + user_names.sample + "/" + title.gsub(/\s+/, "-") + ". Thanks in advance for your feedback!")
   Project.create(title: title, description: description)
 end
 
-content.each do |content|
+project_count.times do |index|
+  ProjectOwner.create(project_id: index + 1, user_id: get_random_item_index(user_names))
+end
+
+100.times do
+  ProjectInvite.create(project_id: rand(project_count) + 1, user_id: get_random_item_index(user_names))
+end
+
+review_content.each do |content|
   Review.create(content: content)
+end
+
+75.times do
+  ProjectReview.create(project_id: rand(project_count) + 1, review_id: get_random_item_index(review_content))
+end
+
+45.times do
+  UserReview.create(user_id: get_random_item_index(user_names), review_id: get_random_item_index(review_content))
 end
 
 20.times do
@@ -88,10 +101,10 @@ end
   end
 end
 
-45.times do
-  ProjectReview.create(project_id: rand(projects.length) + 1, review_id: rand(content.length) + 1)
+75.times do
+  ReviewRating.create(review_id: get_random_item_index(review_content), rating_id: rand(20) + 1)
 end
 
-60.times do
-  ReviewRating.create(review_id: rand(content.length) + 1, rating_id: rand(20) + 1)
+75.times do
+  UserRating.create(user_id: get_random_item_index(user_names), rating_id: rand(20) + 1)
 end
